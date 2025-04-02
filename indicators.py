f26f03ee663bc0a07258e3b9f226a8ec5fa5ac59
Imad Ahmad <imadahmad97@yahoo.ca>
2025-04-01 17:47:33 -0700

    tracking buy/sell/hold state

--- indicators.py
+++ indicators.py
@@ -18,3 +18,30 @@ def calculate_rsi(df: pd.DataFrame, period: int = 14):
     df["rsi"] = 100 - (100 / (1 + rs))
 
     return df
+
+
+def percent_below_ema(df: pd.DataFrame) -> pd.DataFrame:
+    df["percent_below_ema"] = (df["close"] - df["ema_20"]) / df["ema_20"] * 100
+    return df
+
+
+def buy_hold_or_sell(df: pd.DataFrame) -> pd.DataFrame:
+    in_position = False  # We're not in a trade at the start
+    actions = []
+
+    for i, row in df.iterrows():
+        if not in_position:
+            if row["percent_below_ema"] < -0.3 and row["rsi"] < 30:
+                actions.append("buy")
+                in_position = True
+            else:
+                actions.append("hold")
+        else:
+            if row["percent_below_ema"] > 0.3 and row["rsi"] > 70:
+                actions.append("sell")
+                in_position = False
+            else:
+                actions.append("hold")
+
+    df["action"] = actions
+    return df
--- transform_data.py
+++ transform_data.py
@@ -1,4 +1,4 @@
-from indicators import calculate_ema, calculate_rsi
+from indicators import calculate_ema, calculate_rsi, percent_below_ema, buy_hold_or_sell
 import pandas as pd
 
 
@@ -6,4 +6,6 @@ def transform_data(path_to_csv="data/historical_data.csv"):
     df = pd.read_csv(path_to_csv, index_col="date", parse_dates=True)
     df = calculate_ema(df)
     df = calculate_rsi(df)
+    df = percent_below_ema(df)
+    df = buy_hold_or_sell(df)
     return df
